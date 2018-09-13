7674a2a6551729f00971c167f7cf718c4ebd2b60
Emilis Makutėnas <emilis.em4@gmail.com>
2018-09-14 00:33:16 +0300

    Clean project

--- cancer-isp/ClientApp/js/hello.tsx
+++
@@ -1,8 +0,0 @@
-﻿import * as React from "react";
-import * as ReactDom from "react-dom";
-
-import App from "./hello/App";
-
-ReactDom.render(
-    <App />,
-    document.getElementById("react-app"));
--- cancer-isp/ClientApp/js/hello/App.tsx
+++
@@ -1,7 +0,0 @@
-﻿import * as React from "react";
-
-const App = () => {
-    return (<h1>Hello from react!</h1>);
-};
-
-export default App;
