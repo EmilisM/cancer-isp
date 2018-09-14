b1f54b76d5763ee5577cb80b1b86bd5a3cd86694
Emilis Makutėnas <emilis.em4@gmail.com>
2018-09-14 22:38:02 +0300

    Final project init

---
+++ cancer-isp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cancer_isp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index", new[] {"First", "Second"});
+        }
+    }
+}
