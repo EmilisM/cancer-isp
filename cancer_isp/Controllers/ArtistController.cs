76e442f39e9968b237a0d7bd99687d4e63669282
Emilis Makutėnas <emilis.em4@gmail.com>
2018-11-02 20:59:34 +0200

    Add ArtistController, ArtistWorkController pages for those controllers

---
+++ cancer_isp/Controllers/ArtistController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cancer_isp.Controllers
+{
+    public class ArtistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+	}
+}
---
+++ cancer_isp/Controllers/ArtistWorkController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cancer_isp.Controllers
+{
+    public class ArtistWorkController : Controller
+    {
+        [Route("work")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("work/register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
