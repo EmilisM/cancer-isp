715aeda2f99731695a011b46b16c75bd3b6f7101
Aidas <linikas.aidas@gmail.com>
2018-12-01 16:37:18 +0200

    Add artist work service

--- cancer_isp/Controllers/ArtistWorkController.cs
+++ cancer_isp/Controllers/ArtistWorkController.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using cancer_isp.Models;
+using cancer_isp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace cancer_isp.Controllers
 {
     public class ArtistWorkController : BaseController
     {
-        [Route("work")]
-        public IActionResult Index()
+        private readonly IArtistWorkService _artistWorkService;
+        private readonly IArtistService _artistService;
+
+        public ArtistWorkController(IArtistWorkService artistWorkService)
         {
-            return View();
+            _artistWorkService = artistWorkService;
+        }
+
+        [Route("work/{id}")]
+        public IActionResult Index(int id)
+        {
+            var artistWork = _artistWorkService.GetArtistWork(id);
+            var artists = _artistWorkService.GetArtistWorkCreators(id);
+
+            var artistsString = "";
+            foreach (var item in artists)
+            {
+                artistsString += $"{item.Pseudonym}, ";
+            }
+
+            var artistWorkViewModel = new ArtistWorkViewModel()
+            {
+                Artists = artistsString,
+                ArtistWork = artistWork
+            };
+
+            return View(artistWorkViewModel);
         }
 
         [Route("work/register")]
---
+++ cancer_isp/Services/Interfaces/IArtistWorkService.cs
@@ -0,0 +1,11 @@
+﻿using cancer_isp.Models.Dbo;
+using System.Collections.Generic;
+
+namespace cancer_isp.Services.Interfaces
+{
+    public interface IArtistWorkService
+    {
+        ArtistWork GetArtistWork(int id);
+        List<Artist> GetArtistWorkCreators(int artistWorkId);
+    }
+}
