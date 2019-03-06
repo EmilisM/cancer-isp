6b68b7a8e966aecac120a3f39ccd565437ee9be4
Emilis Makutėnas <emilis.em4@gmail.com>
2019-03-06 20:55:20 +0200

    Add component folder for structurization of the project, add Profile.Create and Profile.Create.List pages

--- cancer-isp-2/ClientApp/src/components/Body.js
+++ cancer-isp-2/ClientApp/src/components/Body.js
@@ -1,30 +1,30 @@
 ﻿import React from "react";
 import Container from "react-bootstrap/Container";
 import { Route } from "react-router-dom";
-import Home from "./Home";
-import Profile from "./Profile";
-import SignUp from "./Signup";
-import Login from "./Login";
-import Song from "./Song";
-import Search from "./Search";
-import Artist from "./Artist";
-import Admin from "./Admin";
-import ArtistCreate from "./Artist.Create";
-import SongCreate from "./Song.Create";
+import Home from "./Home/Home";
+import Profile from "./Profile/Profile";
+import SignUp from "./Signup/Signup";
+import Login from "./Login/Login";
+import Song from "./Song/Song";
+import Search from "./Search/Search";
+import Artist from "./Artist/Artist";
+import Admin from "./Admin/Admin";
+import ArtistCreate from "./Artist/Artist.Create";
+import SongCreate from "./Song/Song.Create";
 
 function Body() {
     return (
         <Container fluid="true">
-            <Route exact path="/" component={Home} />
-            <Route exact path="/profile" component={Profile} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/song" component={Song} />
-            <Route exact path="/artist" component={Artist} />
-            <Route exact path="/search" component={Search} />
-            <Route exact path="/admin" component={Admin} />
-            <Route exact path="/artist/create" component={ArtistCreate} />
-            <Route exact path="/song/create" component={SongCreate} />
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/profile" component={Profile}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/signup" component={SignUp}/>
+            <Route exact path="/song" component={Song}/>
+            <Route exact path="/artist" component={Artist}/>
+            <Route exact path="/search" component={Search}/>
+            <Route exact path="/admin" component={Admin}/>
+            <Route exact path="/artist/create" component={ArtistCreate}/>
+            <Route exact path="/song/create" component={SongCreate}/>
         </Container>
     );
 }
