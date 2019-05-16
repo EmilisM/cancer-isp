12f6436f3500f8da43a279ebeedd7a3809209370
Emilis Makutėnas <emilis.em4@gmail.com>
2019-05-16 22:24:19 +0300

    Add database script

--- cancer_isp_2/Properties/cancer_isp_2.sql
+++ cancer_isp_2/Properties/cancer_isp_2.sql
@@ -1,141 +1,535 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 16, 2019 at 09:16 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cancer_isp_2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
 CREATE DATABASE IF NOT EXISTS `cancer_isp_2`;
 
-DROP TABLE IF EXISTS playlist_song;
-DROP TABLE IF EXISTS rating;
-DROP TABLE IF EXISTS artist_album;
-DROP TABLE IF EXISTS artist_song;
-DROP TABLE IF EXISTS artist;
-DROP TABLE IF EXISTS playlist;
-DROP TABLE IF EXISTS song_genre;
-DROP TABLE IF EXISTS song;
-DROP TABLE IF EXISTS album;
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS user_profile;
-DROP TABLE IF EXISTS user_role;
-DROP TABLE IF EXISTS image;
-DROP TABLE IF EXISTS genre;
+USE `cancer_isp_2`;
 
-CREATE TABLE genre (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255),
-  description VARCHAR(255)
-); 
+CREATE TABLE `album` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `release_date` date DEFAULT NULL,
+  `image_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE image (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255),
-  url VARCHAR(255)
-); 
+--
+-- Dumping data for table `album`
+--
 
-CREATE TABLE user_role (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255)
-); 
+INSERT INTO `album` (`id`, `name`, `release_date`, `image_id`) VALUES
+(1, 'Newest and greatest hits', '2019-05-11', NULL),
+(2, 'Bestest hits', '2019-05-15', NULL);
 
-CREATE TABLE user_profile (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  description VARCHAR(255),
-  name VARCHAR(255),
-  last_name VARCHAR(255),
-  birthdate DATE,
-  phone_number VARCHAR(255)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE user (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255),
-  registration_date DATETIME,
-  password_hash VARCHAR(255),
-  password_salt VARCHAR(255),
-  email VARCHAR(255),
-  karma_points INT,
-  user_role_id INT,
-  user_profile_id INT UNIQUE KEY,
-  FOREIGN KEY(user_role_id) REFERENCES user_role(id),
-  FOREIGN KEY(user_profile_id) REFERENCES user_profile(id)
-);
+--
+-- Table structure for table `artist`
+--
 
-CREATE TABLE album (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255),
-  release_date DATE,
-  image_id INT,
-  FOREIGN KEY(image_id) REFERENCES image(id)
-);
+CREATE TABLE `artist` (
+  `id` int(11) NOT NULL,
+  `alias` varchar(255) DEFAULT NULL,
+  `full_name` varchar(255) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `origin_date` date DEFAULT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE song (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255),
-  release_date DATETIME,
-  length_in_seconds INT,
-  description VARCHAR(255),
-  image_id INT,
-  album_id INT,
-  user_id INT,
-  FOREIGN KEY(image_id) REFERENCES image(id),
-  FOREIGN KEY(album_id) REFERENCES album(id),
-  FOREIGN KEY(user_id) REFERENCES user(id)
-); 
+--
+-- Dumping data for table `artist`
+--
 
-CREATE TABLE song_genre (
-  song_id INT NOT NULL,
-  genre_id INT NOT NULL,
-  PRIMARY KEY(song_id, genre_id),
-  FOREIGN KEY(song_id) REFERENCES song(id),
-  FOREIGN KEY(genre_id) REFERENCES genre(id)
-);
+INSERT INTO `artist` (`id`, `alias`, `full_name`, `birthdate`, `description`, `origin_date`, `user_id`) VALUES
+(3, 'Vienas', 'VienasVienas', '2019-05-29', 'Isthis', '2019-05-03', 1),
+(4, 'Du', 'DuDu', '2019-05-16', 'Isthis', '2019-05-01', 1);
 
-CREATE TABLE playlist (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255),
-  description VARCHAR(255),
-  state VARCHAR(255),
-  user_id INT,
-  FOREIGN KEY(user_id) REFERENCES user(id)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE artist (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  alias VARCHAR(255),
-  full_name VARCHAR(255),
-  birthdate DATE,
-  description VARCHAR(255),
-  origin_date DATE,
-  user_id INT NOT NULL,
-  FOREIGN KEY(user_id) REFERENCES user(id)
-);
+--
+-- Table structure for table `artist_album`
+--
 
-CREATE TABLE artist_song (
-  song_id INT NOT NULL,
-  artist_id INT NOT NULL,
-  PRIMARY KEY(song_id, artist_id),
-  FOREIGN KEY(song_id) REFERENCES song(id),
-  FOREIGN KEY(artist_id) REFERENCES artist(id)
-);
+CREATE TABLE `artist_album` (
+  `artist_id` int(11) NOT NULL,
+  `album_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE artist_album (
-  artist_id INT NOT NULL,
-  album_id INT NOT NULL,
-  PRIMARY KEY(artist_id, album_id),
-  FOREIGN KEY(artist_id) REFERENCES artist(id),
-  FOREIGN KEY(album_id) REFERENCES album(id)
-);
+--
+-- Dumping data for table `artist_album`
+--
 
-CREATE TABLE rating (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  points INT,
-  creation_date DATE,
-  comment VARCHAR(255),
-  song_id INT NOT NULL,
-  user_id INT NOT NULL,
-  FOREIGN KEY(song_id) REFERENCES song(id),
-  FOREIGN KEY(user_id) REFERENCES user(id)
-);
+INSERT INTO `artist_album` (`artist_id`, `album_id`) VALUES
+(3, 2),
+(4, 1);
 
-CREATE TABLE playlist_song (
-  playlist_id INT NOT NULL,
-  song_id INT NOT NULL,
-  PRIMARY KEY(playlist_id, song_id),
-  FOREIGN KEY(playlist_id) REFERENCES playlist(id),
-  FOREIGN KEY(song_id) REFERENCES song(id)
-);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artist_song`
+--
+
+CREATE TABLE `artist_song` (
+  `song_id` int(11) NOT NULL,
+  `artist_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `artist_song`
+--
+
+INSERT INTO `artist_song` (`song_id`, `artist_id`) VALUES
+(1, 3),
+(1, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genre`
+--
+
+CREATE TABLE `genre` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `genre`
+--
+
+INSERT INTO `genre` (`id`, `name`, `description`) VALUES
+(1, 'Rock', NULL),
+(2, 'Pop', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image`
+--
+
+CREATE TABLE `image` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `image`
+--
+
+INSERT INTO `image` (`id`, `name`, `url`) VALUES
+(1, 'best song cover', 'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F730871545023725569%2FF41PZU8M.jpg&f=1'),
+(2, 'one two ', 'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.dyQSswy7D6O7yMaZSR6GuQHaHa%26pid%3DApi&f=1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlist`
+--
+
+CREATE TABLE `playlist` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlist_song`
+--
+
+CREATE TABLE `playlist_song` (
+  `playlist_id` int(11) NOT NULL,
+  `song_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `id` int(11) NOT NULL,
+  `points` int(11) DEFAULT NULL,
+  `creation_date` date DEFAULT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `song_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rating`
+--
+
+INSERT INTO `rating` (`id`, `points`, `creation_date`, `comment`, `song_id`, `user_id`) VALUES
+(1, 10, '2019-05-10', 'Very good song!', 1, 1),
+(2, 6, '2019-05-01', 'Sub par', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `song`
+--
+
+CREATE TABLE `song` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `release_date` datetime DEFAULT NULL,
+  `length_in_seconds` int(11) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `image_id` int(11) DEFAULT NULL,
+  `album_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `youtube_video_id` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `song`
+--
+
+INSERT INTO `song` (`id`, `name`, `release_date`, `length_in_seconds`, `description`, `image_id`, `album_id`, `user_id`, `youtube_video_id`) VALUES
+(1, 'First song of album', '2019-05-10 00:00:00', 120, NULL, 1, 2, 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `song_genre`
+--
+
+CREATE TABLE `song_genre` (
+  `song_id` int(11) NOT NULL,
+  `genre_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `song_genre`
+--
+
+INSERT INTO `song_genre` (`song_id`, `genre_id`) VALUES
+(1, 1),
+(1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `registration_date` datetime DEFAULT NULL,
+  `password_hash` varchar(255) DEFAULT NULL,
+  `password_salt` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `karma_points` int(11) DEFAULT NULL,
+  `user_role_id` int(11) DEFAULT NULL,
+  `user_profile_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `registration_date`, `password_hash`, `password_salt`, `email`, `karma_points`, `user_role_id`, `user_profile_id`) VALUES
+(1, 'admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_profile`
+--
+
+CREATE TABLE `user_profile` (
+  `id` int(11) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `image_id` (`image_id`);
+
+--
+-- Indexes for table `artist`
+--
+ALTER TABLE `artist`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `artist_album`
+--
+ALTER TABLE `artist_album`
+  ADD PRIMARY KEY (`artist_id`,`album_id`),
+  ADD KEY `album_id` (`album_id`);
+
+--
+-- Indexes for table `artist_song`
+--
+ALTER TABLE `artist_song`
+  ADD PRIMARY KEY (`song_id`,`artist_id`),
+  ADD KEY `artist_id` (`artist_id`);
+
+--
+-- Indexes for table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `playlist`
+--
+ALTER TABLE `playlist`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `playlist_song`
+--
+ALTER TABLE `playlist_song`
+  ADD PRIMARY KEY (`playlist_id`,`song_id`),
+  ADD KEY `song_id` (`song_id`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `song_id` (`song_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `song`
+--
+ALTER TABLE `song`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `image_id` (`image_id`),
+  ADD KEY `album_id` (`album_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `song_genre`
+--
+ALTER TABLE `song_genre`
+  ADD PRIMARY KEY (`song_id`,`genre_id`),
+  ADD KEY `genre_id` (`genre_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_profile_id` (`user_profile_id`),
+  ADD KEY `user_role_id` (`user_role_id`);
+
+--
+-- Indexes for table `user_profile`
+--
+ALTER TABLE `user_profile`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `album`
+--
+ALTER TABLE `album`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `artist`
+--
+ALTER TABLE `artist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `image`
+--
+ALTER TABLE `image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `playlist`
+--
+ALTER TABLE `playlist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `song`
+--
+ALTER TABLE `song`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_profile`
+--
+ALTER TABLE `user_profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `album`
+--
+ALTER TABLE `album`
+  ADD CONSTRAINT `album_ibfk_1` FOREIGN KEY (`image_id`) REFERENCES `image` (`id`);
+
+--
+-- Constraints for table `artist`
+--
+ALTER TABLE `artist`
+  ADD CONSTRAINT `artist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `artist_album`
+--
+ALTER TABLE `artist_album`
+  ADD CONSTRAINT `artist_album_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`id`),
+  ADD CONSTRAINT `artist_album_ibfk_2` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`);
+
+--
+-- Constraints for table `artist_song`
+--
+ALTER TABLE `artist_song`
+  ADD CONSTRAINT `artist_song_ibfk_1` FOREIGN KEY (`song_id`) REFERENCES `song` (`id`),
+  ADD CONSTRAINT `artist_song_ibfk_2` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`id`);
+
+--
+-- Constraints for table `playlist`
+--
+ALTER TABLE `playlist`
+  ADD CONSTRAINT `playlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `playlist_song`
+--
+ALTER TABLE `playlist_song`
+  ADD CONSTRAINT `playlist_song_ibfk_1` FOREIGN KEY (`playlist_id`) REFERENCES `playlist` (`id`),
+  ADD CONSTRAINT `playlist_song_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `song` (`id`);
+
+--
+-- Constraints for table `rating`
+--
+ALTER TABLE `rating`
+  ADD CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`song_id`) REFERENCES `song` (`id`),
+  ADD CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `song`
+--
+ALTER TABLE `song`
+  ADD CONSTRAINT `song_ibfk_1` FOREIGN KEY (`image_id`) REFERENCES `image` (`id`),
+  ADD CONSTRAINT `song_ibfk_2` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`),
+  ADD CONSTRAINT `song_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `song_genre`
+--
+ALTER TABLE `song_genre`
+  ADD CONSTRAINT `song_genre_ibfk_1` FOREIGN KEY (`song_id`) REFERENCES `song` (`id`),
+  ADD CONSTRAINT `song_genre_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_role_id`) REFERENCES `user_role` (`id`),
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`user_profile_id`) REFERENCES `user_profile` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
