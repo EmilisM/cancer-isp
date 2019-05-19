c11f8993e02aebdeaa8d2e2e02aae63006f29df9
Emilis Makutėnas <emilis.em4@gmail.com>
2019-05-19 13:52:38 +0300

    Update db script

--- cancer_isp_2/Properties/cancer_isp_2.sql
+++ cancer_isp_2/Properties/cancer_isp_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2019 at 08:01 PM
+-- Generation Time: May 19, 2019 at 12:51 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -214,7 +214,9 @@ CREATE TABLE `rating` (
 INSERT INTO `rating` (`id`, `points`, `creation_date`, `comment`, `song_id`, `user_id`) VALUES
 (1, 10, '2019-05-10', 'Very good song!', 1, 1),
 (2, 6, '2019-05-01', 'Sub par', 1, 1),
-(3, 9, '2019-05-17', 'nice', 1, 1);
+(3, 9, '2019-05-17', 'nice', 1, 1),
+(4, 7, '2019-05-18', 'Very nice', 2, 1),
+(5, 8, '2019-05-18', 'Song is nice very', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -311,6 +313,16 @@ CREATE TABLE `user_role` (
   `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `name`) VALUES
+(1, 'user'),
+(2, 'verified_user'),
+(3, 'admin'),
+(4, 'blocked');
 
 --
 -- Indexes for dumped tables
@@ -452,7 +464,7 @@ ALTER TABLE `playlist`
 -- AUTO_INCREMENT for table `rating`
 --
 ALTER TABLE `rating`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `song`
@@ -476,7 +488,7 @@ ALTER TABLE `user_profile`
 -- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
