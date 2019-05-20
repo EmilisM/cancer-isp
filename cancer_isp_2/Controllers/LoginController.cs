5b8f44e9bda2a7eace6495cd84685860f4db7317
Aidas <linikas.aidas@gmail.com>
2019-05-20 16:40:46 +0300

    Add LoginController

---
+++ cancer_isp_2/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using cancer_isp_2.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cancer_isp_2.Controllers
+{
+    [Route("api/login")]
+    [Produces("application/json")]
+    [ApiController]
+    public class LoginController : ControllerBase
+
+    {
+        private readonly CancerIspContext context;
+
+        public LoginController(CancerIspContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetUser(string username)
+        {
+            var newuser = context.User
+                .FirstOrDefault(user => user.Username == username);
+
+            return Ok(newuser);
+        }
+    }
+}
